ca5923b46837c157d0b9de3cbbff2caf91e4f5aa
Jim White <jim@fovi.com>
2025-08-18 13:56:40 -0700

    Using acl2-customization.lisp works

--- context/extension/common-lisp-jupyter.lisp
+++ context/extension/common-lisp-jupyter.lisp
@@ -3,8 +3,8 @@
 
 (include-book "tools/include-raw" :dir :system)
 
-(defttag :quicklisp)
-(include-book "quicklisp/top" :dir :system :ttags :all)
+;(defttag :quicklisp)
+(include-book "quicklisp/cl-fad" :dir :system :ttags :all)
 
 (defttag :common-lisp-jupyter)
 (include-raw "common-lisp-jupyter-raw.lsp" :do-not-compile t)
